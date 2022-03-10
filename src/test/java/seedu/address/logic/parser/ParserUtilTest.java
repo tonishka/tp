c4d93b78c6163d29df582eba4deb5c32e8d7ac8f
Charisma Kausar <charisma.kausar@gmail.com>
2022-03-11 00:10:11 +0800

    Test for empty address input

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -43,7 +43,7 @@ public class ParserUtilTest {
 
     private static final String INVALID_ADDRESS_WITH_LABEL_1 = "l/home";
     private static final String INVALID_ADDRESS_WITH_LABEL_2 = "123 Main Street #0505l/home";
-    private static final String INVALID_ADDRESS_WITH_LABEL_3 = "23 Main Street #0505l/ home";
+    private static final String INVALID_ADDRESS_WITH_LABEL_3 = " ";
 
     private static final String INVALID_EMAIL_WITH_LABEL_2 = "rachel@example.coml/home";
     private static final String INVALID_EMAIL_WITH_LABEL_3 = "rachel@example.coml/ home";
