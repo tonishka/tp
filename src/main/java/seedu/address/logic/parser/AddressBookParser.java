4af8fe976d913ac6bb2fdd33c4792727d3ff8621
NicsunXnus <e0543645@u.nus.edu>
2022-03-14 17:55:18 +0800

    checkstyle fix

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_VIEW;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_EDIT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_VIEW;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import java.util.regex.Matcher;
