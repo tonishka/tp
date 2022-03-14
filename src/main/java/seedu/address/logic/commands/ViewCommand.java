539d43004e41fea3df6c61fbef7958f745b6bbe8
muraddurrani <77261055+muraddurrani@users.noreply.github.com>
2022-03-14 11:52:55 +0800

    Implement ViewCommand

---
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -0,0 +1,52 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+
+/**
+ * Views the full contact details of the specified person.
+ */
+public class ViewCommand extends Command {
+
+    public static final String COMMAND_WORD = "view";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays the contact details of the person identified "
+            + "by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1\n";
+
+    public static final String SHOWING_CONTACT_INFO = "Showing contact information for person: %1$s";
+
+    private final Index index;
+
+    /**
+     * @param index of the person in the filtered person list to display
+     */
+    public ViewCommand(Index index) {
+        requireNonNull(index);
+
+        this.index = index;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person toDisplay = lastShownList.get(index.getZeroBased());
+
+        return new CommandResult(String.format(SHOWING_CONTACT_INFO, toDisplay), false, false,
+                false, true, toDisplay);
+    }
+}
