1abc1ac88df26ba0f5d3af931c464e09f54c2a61
tonishka <tonishkasingh21@gmail.com>
2022-03-25 03:59:05 +0800

    Add MeetCommand Class

---
+++ src/main/java/seedu/address/logic/commands/MeetCommand.java
@@ -0,0 +1,6 @@
+package seedu.address.logic.commands;
+
+public class MeetCommand {
+    public static final String COMMAND_WORD = "back";
+    public static final String MESSAGE_SUCCESS = "Returned to all contacts!";
+}
