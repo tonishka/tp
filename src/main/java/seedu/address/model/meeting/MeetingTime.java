02b415b507d96c1285b015080345b913fd443b19
Charisma Kausar <charisma.kausar@gmail.com>
2022-03-27 09:55:56 +0800

    Check if meeting time is valid

--- src/main/java/seedu/address/model/meeting/MeetingTime.java
+++ src/main/java/seedu/address/model/meeting/MeetingTime.java
@@ -1,9 +1,11 @@
 package seedu.address.model.meeting;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Represents the meeting time for a meeting.
@@ -12,22 +14,33 @@ public class MeetingTime {
     public static final String MESSAGE_CONSTRAINTS =
             "Meeting time must be in the following format: YYYY-MM-dd HH:mm\n Example: 2022-05-25 23:59";
     public final LocalDateTime dateTime;
-    private final DateTimeFormatter formatter;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
     /**
-     * Creates a new meeting time
-     * @param dateTime
+     * Creates a new meeting time.
+     * @param dateTime Meeting date and time.
      */
     public MeetingTime(LocalDateTime dateTime) {
         requireNonNull(dateTime);
-        this.formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        checkArgument(isValidMeetingTime(String.valueOf(dateTime)), MESSAGE_CONSTRAINTS);
         this.dateTime = dateTime;
+    }
+
+    /**
+     * Returns true if a given string is a valid meeting time.
+     */
+    public static boolean isValidMeetingTime(String test) {
+        try {
+            LocalDateTime.parse(test, formatter);
+            return true;
+        } catch (DateTimeParseException e){
+            return false;
+        }
     }
 
     @Override
     public String toString() {
-        String formatted = dateTime.format(formatter);
-        return formatted;
+        return dateTime.format(formatter);
     }
 
     @Override
