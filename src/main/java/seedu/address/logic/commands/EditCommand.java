25cbd2439f1344c01a06a271a8ca66375d795a82
NicsunXnus <e0543645@u.nus.edu>
2022-03-28 22:17:09 +0800

    commit

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -80,6 +80,7 @@ public class EditCommand extends Command {
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
         return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson.getName().fullName),
                 false, false, false, true, false,
                 editedPerson);
