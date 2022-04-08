ea3395dc6c2c82f56b3a615e20af468f3f00df96
muraddurrani <77261055+muraddurrani@users.noreply.github.com>
2022-04-08 13:06:04 +0800

    Combine PersonCommandTestUtil and MeetingCommandTestUtil

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -1,6 +1,6 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.PersonCommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalMeetings.getTypicalMeetingBook;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
