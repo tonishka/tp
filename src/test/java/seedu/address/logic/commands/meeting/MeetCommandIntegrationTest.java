880f954058f917ee04ca259d501b6c007bfc9cb3
tonishka <tonishkasingh21@gmail.com>
2022-04-09 16:36:50 +0800

    Fix import order

--- src/test/java/seedu/address/logic/commands/meeting/MeetCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/meeting/MeetCommandIntegrationTest.java
@@ -1,16 +1,17 @@
 package seedu.address.logic.commands.meeting;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalMeetings.getTypicalMeetingBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.meeting.Meeting;
 import seedu.address.testutil.MeetingBuilder;
-
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalMeetings.getTypicalMeetingBook;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code MeetCommand}.
--- src/test/java/seedu/address/logic/commands/meeting/MeetCommandTest.java
+++ src/test/java/seedu/address/logic/commands/meeting/MeetCommandTest.java
@@ -6,20 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
-import javafx.collections.ObservableList;
-import org.junit.jupiter.api.Test;
-
-import seedu.address.commons.core.GuiSettings;
-import seedu.address.logic.commands.CommandResult;
-import seedu.address.model.*;
-import seedu.address.model.meeting.Meeting;
-import seedu.address.model.person.Person;
-import seedu.address.testutil.MeetingBuilder;
-
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.AddressBook;
+import seedu.address.model.MeetingBook;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyMeetingBook;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.meeting.Meeting;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.MeetingBuilder;
 
 public class MeetCommandTest {
     @Test
