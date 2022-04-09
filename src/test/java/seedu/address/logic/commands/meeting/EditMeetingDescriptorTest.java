8811b498ec3b8ae21ed81740966876bf1da5530f
muraddurrani <77261055+muraddurrani@users.noreply.github.com>
2022-04-09 20:20:24 +0800

    Add tests for UpdateMeetingDescriptor

--- src/test/java/seedu/address/logic/commands/meeting/EditMeetingDescriptorTest.java
+++
@@ -1,4 +0,0 @@
-package seedu.address.logic.commands.meeting;
-
-public class EditMeetingDescriptorTest {
-}
---
+++ src/test/java/seedu/address/logic/commands/meeting/UpdateMeetingDescriptorTest.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.commands.meeting;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_PROJECT;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_QUARTERLY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_AGENDA_PROJECT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PLACE_PROJECT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TIME_PROJECT;
+
+import seedu.address.model.meeting.UpdateMeetingDescriptor;
+import seedu.address.testutil.UpdateMeetingDescriptorBuilder;
+
+import org.junit.jupiter.api.Test;
+
+public class UpdateMeetingDescriptorTest {
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        UpdateMeetingDescriptor descriptorWithSameValues = new UpdateMeetingDescriptor(DESC_QUARTERLY);
+        assertTrue(DESC_QUARTERLY.equals(descriptorWithSameValues));
+
+        // same object -> returns true
+        assertTrue(DESC_QUARTERLY.equals(DESC_QUARTERLY));
+
+        // null -> returns false
+        assertFalse(DESC_QUARTERLY.equals(null));
+
+        // different types -> returns false
+        assertFalse(DESC_QUARTERLY.equals(5));
+
+        // different values -> returns false
+        assertFalse(DESC_QUARTERLY.equals(DESC_PROJECT));
+
+        // different agenda -> returns false
+        UpdateMeetingDescriptor editedQuarterly = new UpdateMeetingDescriptorBuilder(DESC_QUARTERLY)
+                .withAgenda(VALID_AGENDA_PROJECT).build();
+        assertFalse(DESC_QUARTERLY.equals(editedQuarterly));
+
+        // different meeting time -> returns false
+        editedQuarterly = new UpdateMeetingDescriptorBuilder(DESC_QUARTERLY)
+                .withTime(VALID_TIME_PROJECT).build();
+        assertFalse(DESC_QUARTERLY.equals(editedQuarterly));
+
+        // different meeting place -> returns false
+        editedQuarterly = new UpdateMeetingDescriptorBuilder(DESC_QUARTERLY)
+                .withPlace(VALID_PLACE_PROJECT).build();
+        assertFalse(DESC_QUARTERLY.equals(editedQuarterly));
+    }
+}
