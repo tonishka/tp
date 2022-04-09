02119b63c8551e609ee1a2d8c1619a88690cb867
tonishka <tonishkasingh21@gmail.com>
2022-04-09 14:00:40 +0800

    Add Meeting Integration tests

---
+++ src/test/java/seedu/address/logic/commands/meeting/MeetCommandIntegrationTest.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.commands.meeting;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.meeting.Meeting;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.MeetingBuilder;
+import seedu.address.testutil.PersonBuilder;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalMeetings.getTypicalMeetingBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code MeetCommand}.
+ */
+public class MeetCommandIntegrationTest {
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), getTypicalMeetingBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_newMeeting_success() {
+        Meeting validMeeting = new MeetingBuilder().build();
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), model.getMeetingBook(), new UserPrefs());
+        expectedModel.addMeeting(validMeeting);
+
+        assertCommandSuccess(new MeetCommand(validMeeting), model,
+                String.format(MeetCommand.MESSAGE_SUCCESS), expectedModel);
+    }
+}
